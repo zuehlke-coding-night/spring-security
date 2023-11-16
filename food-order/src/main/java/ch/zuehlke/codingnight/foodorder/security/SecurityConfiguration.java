3df2c07d5992ebf296518a8e8580797601b66a3c
Marco Romanutti <m.romanutti@gmail.com>
2023-11-16 20:47:30 +0100

    Add okta idp

--- food-order/src/main/java/ch/zuehlke/codingnight/foodorder/security/SecurityConfiguration.java
+++ food-order/src/main/java/ch/zuehlke/codingnight/foodorder/security/SecurityConfiguration.java
@@ -4,19 +4,13 @@ import ch.zuehlke.codingnight.foodorder.filter.AuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
-
-    private final AuthenticationFilter authenticationFilter;
-
-    @Autowired
-    public SecurityConfiguration(AuthenticationFilter authenticationFilter) {
-        this.authenticationFilter = authenticationFilter;
-    }
 
     @Bean
     public SecurityFilterChain getSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -26,6 +20,9 @@ public class SecurityConfiguration {
                     auth
                             .requestMatchers("/api/**").authenticated()
                             .anyRequest().permitAll();
+                })
+                .oauth2ResourceServer(auth -> {
+                    auth.jwt(Customizer.withDefaults());
                 });
 
         return http.build();
